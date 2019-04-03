7c3bed83691f714722013ebbf69b5474ed1b9307
sabrina mianne and stephen trewick <stephen@>
2019-04-03 16:47:35 -0700

    updates

--- js/scripts.js
+++ js/scripts.js
@@ -10,30 +10,32 @@ $(document).ready(function(){
     var regex = /[aeiouAEIOU]/gi ;
     var vowels = ["a","e","i","o","u"];
     var vowelIndex;
+    var befV2= "";
+    var result= "";
 
     var array = inputUser.split(' ');
 //for vowels
     for (var i = 0; i< array.length; i++) {
     var matchV = array[i].slice(0,1).match(regex);
-    if (matchV) { 
+    if (matchV) {
      result = newstr +=  array[i] + way;
 // for consonants
-    } else {
-    for (let char of inputUser) {
-      if (vowels.includes(char)){
-        vowelIndex= inputUser.indexOf(char);
-         var befV = inputUser.slice(0,vowelIndex) ;
-         break
+    } if  (!matchV) {
+      for (var i = 0; i< array.length; i++) {
+        for (let char of inputUser) {
+          if (vowels.includes(char)){
+            vowelIndex= inputUser.indexOf(char);
+            var matchV = array[i].slice(0,vowelIndex);
+            break
           }
-          }
-         result = inputUser.replace(befV,"") + befV + "ay"
-         console.log( result)
+        }
+            var string = array[i];
+            result = newstr +=  string.replace(matchV,"")+ matchV + "ay ";
+}
 }
 }
     $("#finalResult").append(result);
     $("#hidden").show();
 
-
   });
-
 });
